11f42f3b0850d776daf1378ec21ab618f8de02bd
shreyas129 <shreyas.tathode129@gmail.com>
2023-07-29 19:31:55 +0530

    first commit

---
+++ src/Hello100.java
@@ -0,0 +1,21 @@
+sealed class A100 permits B100, C100{
+
+}
+
+class B100 extends A100{
+
+}
+
+class C100 {
+
+}
+
+class D100 {
+
+}
+
+public class Hello100 {
+    public static void main(String a[]) m{
+
+    }
+}
