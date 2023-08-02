f98a055027a6c7592c4a5ca289d3c605b7cb44e3
shreyas129 <shreyas.tathode129@gmail.com>
2023-08-02 19:10:44 +0530

    Kommitatah

--- src/Hello100.java
+++ src/Hello100.java
@@ -1,21 +1,29 @@
-sealed class A100 permits B100, C100{
+sealed class A100 extends Thread implements Cloneable permits B100, C100{
 
 }
 
-class B100 extends A100{
+non-sealed class B100 extends A100 {
 
 }
 
-class C100 {
+final class C100 extends A100 {
 
 }
 
-class D100 {
+class D100 extends B100 {
+
+}
+
+sealed interface x permits y{
+
+}
+
+non-sealed interface y extends x {
 
 }
 
 public class Hello100 {
-    public static void main(String a[]) m{
+    public static void main(String a[]) {
 
     }
 }
--- src/Hello101.java
+++ src/Hello101.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Hello101 {
+import java.util.Objects;
+
+// class Alien {
+//     private final int id;
+//     private final String name;
+//
+//     public Alien(int id, String name) {
+//         this.id = id;
+//         this.name = name;
+//     }
+//
+//     public int getId() {
+//         return id;
+//     }
+//
+//     public String getName() {
+//         return name;
+//     }
+//
+//     @Override
+//     public boolean equals(Object o) {
+//         if (this == o) return true;
+//         if (!(o instanceof Alien alien)) return false;
+//         return getId() == alien.getId() && Objects.equals(getName(), alien.getName());
+//     }
+//
+//     @Override
+//     public int hashCode() {
+//         return Objects.hash(getId(), getName());
+//     }
+//
+//     public String toString() {
+//         return getName() + " : " + getId();
+//     }
+// }
+
+record Alien(int id, String name) {}
+
+public class Hello101 {
+    public static void main(String a[]) {
+        Alien a1 = new Alien(1, "Shreyas");
+        Alien a2 = new Alien(1, "Shreyas");
+
+        System.out.println(a1.equals(a2));
+        System.out.println(a1);
+    }
 }
