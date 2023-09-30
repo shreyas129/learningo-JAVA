3f10c5edccf62d2ce6eea8fd55f9a1b68fbb7024
shreyas129 <shreyas.tathode129@gmail.com>
2023-09-30 14:50:42 +0530

    commit

--- src/tricks/PrimeNum.java
+++ src/tricks/PrimeNum.java
@@ -1,8 +1,8 @@
 package tricks;
 
 public class PrimeNum {
-    public static void main(String a[]) {
-        for(int i = 1; i < 99999999; i++) {
+    public static void main(String[] args) {
+        for(int i = 1; i < 99999; i++) {
             for(int j = 1; j <= i; j++) {
                 if(i == j) {
                     System.out.println(i);
@@ -15,3 +15,4 @@ public class PrimeNum {
         }
     }
 }
+
